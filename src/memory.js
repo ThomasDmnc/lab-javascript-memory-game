0ca654c315b9ae98e59fb2cbb151c0167ba02d9c
thomas demoncy <demoncy.thomas@gmail.com>
2023-09-13 16:00:20 +0200

    finished the class methods.

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,41 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (typeof this.cards === "undefined") return undefined;
+    var setCards = JSON.parse(JSON.stringify(this.cards));
+    var shuffledCards = [];
+
+    for (let i = setCards.length - 1 ; i >= 0; i--) {
+      var randomIndex = Math.floor(Math.random() * setCards.length);
+      shuffledCards.push(setCards[randomIndex]);
+      setCards.splice(randomIndex, 1);
+    }
+
+    this.cards = shuffledCards;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+    if (card1 === card2){
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    };
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed == (this.cards.length / 2)) {
+      return true
+    } else {
+      return false
+    }
   }
 }
